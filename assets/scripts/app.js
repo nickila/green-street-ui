f38070ecb986f3c576c404df25357e7f83f65ff7
Brian Nickila <brian.nickila@gmail.com>
2019-06-08 21:43:12 -0500

    Finished modals for login and register.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
     $(".bar-container").click(function () {
         $(this).toggleClass("change");
+        $(".link-container").toggleClass("show", 500);
+    })
+    $(".link").click(function () {
+        $(".link-container").toggleClass("show", 500);
+        $(".bar-container").toggleClass("change");
+    })
 
-        $(".link-container").toggleClass("display").toggleClass("show", 500);
+    $(".login").click(function () {
+        $(".login-modal").addClass("modal-show");
+    })
+    $(".join").click(function () {
+        $(".join-modal").addClass("modal-show");
+    })
+    $(".close").click(function () {
+        $(this).parent().parent().removeClass("modal-show");
     })
 })
